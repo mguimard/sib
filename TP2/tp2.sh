bc0608ea4937cd6e5d77009c2ef1fd357c30a5d2
Morgan Guimard <morgan.guimard@docdoku.com>
2025-06-30 12:01:28 +0200

    tp2 review

--- TP2/tp2.sh
+++ TP2/tp2.sh
@@ -3,18 +3,17 @@ set -e
 set -x
 
 
-# Changer le "motd" (/etc/motd) (module copy)
+# Ensure the motd content is the specified one
 ansible all -m copy -a "content='Hello World !!!' dest=/etc/motd" --become
 
-# Installer apache grâce au module yum ou apt (module package ou apt ou dnf)
+# Ensure that apache package is present
 ansible machines_debian -m package -a "name=apache2 state=present" --become 
 ansible machines_centos -m package -a "name=httpd state=present" --become 
 
-# Copier un fichier de la machine cliente vers les machines distantes (fichier html à placer dans /var/www/html/) (module copy)
+# Ensure that the content of /var/www/html/index.html is correct
 ansible all -m copy -a "content='Hello World !!!' dest=/var/www/html/index.html" --become
 
-
-# Démarrer apache (module service)
+# Ensure apache service is started
 ansible machines_debian -m service -a "name=apache2 state=started" --become 
 ansible machines_centos -m service -a "name=httpd state=started enabled=true" --become 
 
